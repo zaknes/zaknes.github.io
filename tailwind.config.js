f154e56788a75fede3babec0cc566f79560a8fa6
Zak Nesler <zaknesler@gmail.com>
2019-08-19 14:56:23 -0400

    Use Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,45 @@
 module.exports = {
   theme: {
-    linearGradients: theme => ({
-      directions: {
-        'r': 'to right'
-      },
-      colors: {
-        brand: [theme('colors.brand.200'), theme('colors.brand.300')]
-      },
-    }),
     extend: {
+      fontFamily: {
+        sans: [
+          'Inter',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
       colors: {
         brand: {
           100: '#1732b8',
           200: '#2f4fed',
-          300: '#2f9fed'
+          300: '#2f9fed',
         },
         selection: {
           bg: 'hsl(230, 75%, 95%)',
-          text: 'hsl(230, 84%, 50%)'
-        }
-      }
-    }
+          text: 'hsl(230, 84%, 50%)',
+        },
+      },
+    },
+    linearGradients: theme => ({
+      directions: {
+        'r': 'to right',
+      },
+      colors: {
+        brand: [theme('colors.brand.200'), theme('colors.brand.300')],
+      },
+    }),
   },
   plugins: [
-    require('tailwindcss-gradients')()
-  ]
+    require('tailwindcss-gradients')(),
+  ],
 }
